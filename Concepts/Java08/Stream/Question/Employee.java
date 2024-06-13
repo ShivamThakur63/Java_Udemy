75ef161bbd0baf28b68d7f0fa1b0b4d99c2a8d23
Shivam Thakur <thakurshivam202063@gmail.com>
2024-06-13 20:18:14 +0545

    Exercise Of Stream

--- Concepts/Java08/Stream/Question/Employee.java
+++ Concepts/Java08/Stream/Question/Employee.java
@@ -1,6 +1,7 @@
 package Concepts.Java08.Stream.Question;
 
 import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
@@ -62,11 +63,12 @@ public class Employee {
         Project project1 = new Project("Project1", 2, Arrays.asList(task1, task2));
         Project project2 = new Project("Project2", 6, Arrays.asList(task3, task4, task5, task6, task7));
         Project project3 = new Project("Project3", 4, Arrays.asList(task8, task9, task10));
-        Project project4 = new Project("Project3", 4, Arrays.asList(task11));
+        Project project4 = new Project("Project4", 4, Arrays.asList(task11));
 
-        Employee employee1 = new Employee("Shivam Thakur", Arrays.asList(project1, project2));
-        Employee employee2 = new Employee("Shivam Thakur", Arrays.asList(project3, project4));
-        Employee employee3 = new Employee("Shivam Thakur", Arrays.asList(project4));
+        Employee employee1 = new Employee("Shivam Thakur    ", Arrays.asList(project1, project2));
+        Employee employee2 = new Employee("Pushkar Sah      ", Arrays.asList(project3, project4));
+        Employee employee3 = new Employee("Binayak Prajapti ", Arrays.asList(project4));
+        Employee employee4 = new Employee("Sandesh Pandey   ", new ArrayList<>());
 
         System.out.println("\n\n" + "--".repeat(10) + " First Employee " + "--".repeat(10) + "\n");
 
@@ -92,10 +94,19 @@ public class Employee {
         System.out.println("Employee 2: " + tasksByCategoryEmployee2);
         System.out.println("Employee 3: " + tasksByCategoryEmployee3);
 
-        System.out.println(employee1.getListOfProject().stream().flatMap(Project -> Project.getListOfTask().stream()).collect(Collectors.toList()));
+        // System.out.println(employee1.getListOfProject().stream().map(Project -> Project.getListOfTask().stream()).collect(Collectors.toList()));
+        System.out.println("--".repeat(20));
 
+        employee1.getProjectTime();
+        System.out.println("--".repeat(20));
+        employee2.getProjectTime();
+
+        System.out.println();
+        employee2.getHighestTimeTakingProject();
+        employee4.getHighestTimeTakingProject();
+        System.out.println("--".repeat(20));
+        employee1.TotalTimeNeededToCompleteTask();
     
-
     }
 
     private static void TotalTimeNeededToCompleteTask(Employee employee) {
@@ -107,6 +118,8 @@ public class Employee {
 
         System.out.println("List Of Time Taken Individually : " + Arrays.toString(EmployeeTimeArray));
         System.out.println(("Total Time Needed : " + EmployeeTime));
+
+        
 
 
     }
@@ -130,6 +143,82 @@ public class Employee {
         , Collectors.counting()));
     }
 
+    // The total estimated hours required to complete all tasks in each project for each employee.
+
+    private void  getProjectTime()
+    {
+        List<Project> projectTime = getListOfProject().stream().collect(Collectors.toList());
+        
+        projectTime.forEach(task -> 
+        {
+           int timeTaken = task.getListOfTask().stream().mapToInt(ask -> ask.getDuration()).sum();
+
+            System.out.println("Employee: " + getName() + ", Project: " + task.getName() +
+            ", Total Time Needed: " + timeTaken);
+        });
+        
+    }
+
+    private void getHighestTimeTakingProject() 
+    {
+        List<Project> projectList = getListOfProject().stream().collect(Collectors.toList());
+        int min = 0;
+        int max = 0;
+        String leastTimeTakingProject  = null ;
+        String mostTimeTakingProject  = null ;
+
+
+        if(projectList.isEmpty())
+        {
+            System.out.println("\nNo Projects Found in the List");
+            return;
+        }        
+
+        for (Project project : projectList) {
+            int sum = project.getListOfTask().stream().mapToInt(Task :: getDuration).sum();
+            int temp = 0;
+
+
+            if(sum > max)
+            {
+                temp = max;
+                max = sum;
+                min = temp;
+                mostTimeTakingProject = project.getName();
+            }
+            else if( sum < max)
+            {
+                min = sum;
+                leastTimeTakingProject = project.getName();
+            }
+            
+        }
+        System.out.println("--".repeat(8)+" Project Summary "+"--".repeat(8)+"\n");
+        System.out.println("Most Time Taking Project   : "+mostTimeTakingProject+"\nTime Consumed : " + max);
+        System.out.println("--".repeat(20));
+        System.out.println("Least Time Taking Project  : "+leastTimeTakingProject+"\nTime Consumed : " + min );
+    }
+
+
+    private void TotalTimeNeededToCompleteTask()
+    {
+        Map<String,List<Task>> a = getListOfProject().stream().flatMap(Project -> Project.getListOfTask().stream()).collect(Collectors.groupingBy(task -> {
+            int duration = (int)((Task) task).getDuration();
+                if(duration <= 5 ){
+                return "Low";
+                }else if(duration <= 10 )
+                {
+                    return "Average";
+                }
+                else{
+                    return "High";
+                }
+            }
+            , Collectors.toList()));
+
+
+            System.out.println(a);
+        }
     
 
 }
